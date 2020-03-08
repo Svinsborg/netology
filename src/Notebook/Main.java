689668a6f8413ca474a33af670e1847cd743757b
Svinsborg <61936514+Svinsborg@users.noreply.github.com>
2020-03-08 19:47:06 +0300

    NY add

--- src/Notebook/Main.java
+++ src/Notebook/Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 //import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+// новй год!
+
 
 public class Main {
     public static void main(String[] args) {
