9684299b35c1422bf829c3f774e10e0e9070ba21
Svinsborg <61936514+Svinsborg@users.noreply.github.com>
2020-03-22 20:06:48 +0300

    етить колотить!

--- src/University/Main.java
+++ src/University/Main.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public class Main {
 
+    static final String sEnd = "end";
     static Set<Student> students = new HashSet<>();
     static Scanner scanner = new Scanner(System.in);
 
@@ -23,23 +24,47 @@ public class Main {
                     "#######################################\n");
             int input = Integer.parseInt(scanner.nextLine());
 
-            switch (input){
+            switch (input) {
                 case 1:
-
+                    addStudent();
                     break;
                 case 2:
-
+                    printStudent();
                     break;
-                case 3:
-                    break;
-
                 default:
                     System.exit(0);
                     break;
-
             }
         }
+    }
 
+    private static void printStudent() {
+        for (Student s : students) {
+            System.out.println(s.toString());
+        }
+    }
 
+    private static boolean getStr(String out, StringBuffer got) {
+        System.out.println(out);
+        got.replace(0, got.length(), scanner.nextLine());
+        if (sEnd.equals(got.toString())) return true;
+        return false;
+    }
+
+    private static void addStudent() {
+        StringBuffer input = new StringBuffer();
+        while (true) {
+            if ( getStr("Введите данные студента \n ФИО, группа, № студ. билета", input ));
+            break;
+
+            Scanner buf = new Scanner(input.toString());
+            buf.useDelimiter(",");
+            Student add = new Student(buf.next().trim(), buf.next().trim(), buf.next().trim());
+            buf.close();
+
+            if (students.contains(add)) {
+                System.out.println("Студент существует \n проверть вводимые данные!");
+            } else students.add(add);
+        }
     }
 }
